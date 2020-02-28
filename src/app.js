8b346fad579dc2381222f0fa2993c2d30c32be11
Tim Shaker <tim@tmshkr.com>
2020-02-28 10:51:58 -0800

    removed static data

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
-import React, { useEffect } from "react";
-// import axios from "axios";
-import data from "./data";
+import React from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -13,12 +11,6 @@ import Home from "./components/home";
 import CharacterList from "./components/character-list";
 
 function App() {
-  // useEffect(() => {
-  //   const url = `https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/`;
-  //   axios.get(url).then(response => console.log(response));
-  //   console.log(data);
-  // }, []);
-
   return (
     <div id="app">
       <Router>
@@ -29,10 +21,10 @@ function App() {
               <Home />
             </Route>
             <Route exact path="/characters">
-              <CharacterList data={data} />
+              <CharacterList />
             </Route>
             <Route path="/characters/search/:searchTerm">
-              <CharacterList data={data} />
+              <CharacterList />
             </Route>
             <Redirect to="/" />
           </Switch>
--- src/components/character-list.js
+++ src/components/character-list.js
@@ -5,32 +5,48 @@ import ReactPaginate from "react-paginate";
 
 import CharacterCard from "./character-card";
 
-function CharacterList(props) {
+let prevSearchTerm = "";
+
+function CharacterList() {
   const [page, setPage] = useState(1);
   const [pages, setPages] = useState(5);
-  const [searchResults, setSearchResults] = useState([]);
+  const [fetchedData, setFetchedData] = useState({});
   const [error, setError] = useState("");
-  const { data } = props;
   const { searchTerm } = useParams();
 
   useEffect(() => {
     console.log("useEffect fired", Date.now());
-    // To deal with 429 (Too Many Requests):
-    // Search results from the API are displayed when it works.
-    // If it doesn't work, it falls back to static data.
     if (searchTerm) {
-      const url = `https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/?page=${page}&name=${searchTerm}`;
+      const url = `https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/?page=${
+        searchTerm === prevSearchTerm ? page : 1
+      }&name=${searchTerm}`;
+      prevSearchTerm = searchTerm;
 
       axios
         .get(url)
         .then(({ data }) => {
           console.log(data);
           setPages(data.info.pages);
-          setSearchResults(data.results);
+          setFetchedData(data);
         })
         .catch(err => {
           console.dir(err);
-          setSearchResults([]);
+          setFetchedData([]);
+          setError("There was a problem fetching the data");
+        });
+    } else if (!error) {
+      const url = `https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/?page=${page}`;
+
+      axios
+        .get(url)
+        .then(({ data }) => {
+          console.log(data);
+          setPages(data.info.pages);
+          setFetchedData(data);
+        })
+        .catch(err => {
+          console.dir(err);
+          setFetchedData([]);
           setError("There was a problem fetching the data");
         });
     } else if (pages !== 5) {
@@ -42,22 +58,20 @@ function CharacterList(props) {
     setPage(selected + 1);
   };
 
-  let characters = data[page - 1].results;
-  if (searchTerm && !error && searchResults.length > 0) {
-    characters = searchResults;
-  } else if (searchTerm) {
-    characters = characters.filter(c =>
-      c.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+  let characters = [];
+  if (!error && fetchedData.results) {
+    characters = fetchedData.results;
   }
 
   return (
     <div className="character-list">
       <h2>Characters</h2>
       <div className="list">
-        {characters.map(c => (
-          <CharacterCard key={c.id} character={c} />
-        ))}
+        {error ? (
+          <p>{error}</p>
+        ) : (
+          characters.map(c => <CharacterCard key={c.id} character={c} />)
+        )}
       </div>
       <nav className="pagination" aria-label="Pagination">
         <ReactPaginate
