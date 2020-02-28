1a21f87248dc84cbe436304e5f00249369321394
Tim Shaker <tim@tmshkr.com>
2020-02-28 09:04:38 -0800

    update header

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import SearchBar from "./search-bar";
+
 function Header() {
   return (
     <header className="top">
-      <h1>
-        <Link to="/">Rick & Morty</Link>
-      </h1>
-      <Link to="/characters">Characters</Link>
+      <nav>
+        <Link to="/">Welcome</Link>
+        <Link to="/characters">Characters</Link>
+      </nav>
+      <SearchBar />
     </header>
   );
 }
